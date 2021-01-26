94358682aff6de98bdc640a70510417394d69b1f
Bogdan Iftoda <Bogdan@gmail.com>
2021-01-26 21:56:13 +0200

    asd

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
@@ -21,3 +22,7 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',publish__year=year,
                              publish__month=month,publish__day=day)
     return render(request,'blog/post/detail.html',{'post': post})
+
+
+def home(request):
+     return HttpResponse('<h1>Hello<h1>')
