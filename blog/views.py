e610b12b4e264eca0e1cdbd173e61cb447c87f30
Bogdan Iftoda <Bogdan@gmail.com>
2021-01-26 21:57:45 +0200

    Revert "asd"
    This reverts commit 94358682aff6de98bdc640a70510417394d69b1f.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
@@ -22,7 +21,3 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',publish__year=year,
                              publish__month=month,publish__day=day)
     return render(request,'blog/post/detail.html',{'post': post})
-
-
-def home(request):
-     return HttpResponse('<h1>Hello<h1>')
